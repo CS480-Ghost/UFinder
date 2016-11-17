d5dba7d21b5fd3181e2a90779346f6979e4d294e
Caleb Jang <Calebjang101@gmail.com>
2016-11-16 20:04:17 -0800

    Added new map items, added a clickable marker and display when its clicked

--- WebContent/HTML/Maps.js
+++ WebContent/HTML/Maps.js
@@ -13,6 +13,39 @@ function myMap() {
     zoom: 16
   }
   var map = new google.maps.Map(mapCanvas, mapOptions);
+ 
+ 
+ //THIS IS A TEST MARKER
+  var marker = new google.maps.Marker({
+        position : {lat: 34.058524, lng: -117.824785},
+        map : map,
+        clickable : true
+      });
+  
+  // Get the modal
+var modal = document.getElementById('myModal');
+
+// closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// open the modal on click
+marker.addListener('click', function(){
+    modal.style.display = "block";
+    });
+
+// close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// close it when clicked outside of modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//THIS WAS A TEST MARKER  
+
   
 }
 
@@ -23,14 +56,52 @@ function bathroomFunction() {
   /*finds the map and resets all of it again*/
   
   /*puts markers on where you want*/
-   br1 = {lat: 34.058524, lng: -117.824785}; <!-- to display markers on specific places-->
-   br2 = {lat: 34.057617, lng: -117.823385};
-   br3 = {lat: 34.057857, lng: -117.821132};
-   br4 = {lat: 34.059584, lng: -117.824255};
-   var br = [br1,br2,br3,br4];
+  var locations = [   <!-- to display markers on specific places-->
+  [34.058524,-117.824785, "https://scontent.xx.fbcdn.net/t31.0-8/q82/p960x960/12615594_1101749653210976_8751263346063708335_o.jpg"],
+  [34.057617,-117.823385, "https://scontent.xx.fbcdn.net/v/t1.0-1/c1.0.719.719/13511031_10209986551730399_414828524990737921_n.jpg?oh=0dfa548e22dfd292275245fc363d9d0d&oe=58A41DA1"],
+  [34.057857, -117.821132, "https://scontent.xx.fbcdn.net/v/t1.0-9/14212166_1232232150130419_8485250524428682934_n.jpg?oh=74eb3ddef14b80dd6e0503640a018212&oe=58D38A2A"],
+  [34.059584,-117.824255, "https://scontent.xx.fbcdn.net/v/t1.0-9/10580050_10203823057004547_4192991177799506923_n.jpg?oh=342a30958c834023a0268df58ddc6e45&oe=58A8B09F"]
+  ];
+   var marker;
    for (i = 0; i < 4; i++){
-		 new google.maps.Marker({position: br[i], map: map});
+		marker = new google.maps.Marker({position: new google.maps.LatLng(locations[i][0], locations[i][1]), map: map, clickable: true});
+	
+	
+//ADDED BATHROOM MODAL ITEMS, STILL WORKING ON CHANGING THE PICTURES PER MARKER, I CAN PROBS FIX THAT
+//UNLESS YOU WANNA CHANGE IT	
+	
+//gets modal
+var modal = document.getElementById('myModal');
+
+// makes a close modal and adds a picture change variable
+var span = document.getElementsByClassName("close")[0];
+var picChange = document.getElementById('image');
+
+//open the modal
+marker.addListener('click', function(){
+	picChange.src = locations[1][2]; //need to use i location, location[i][2], like that
+    modal.style.display = "block";
+    });
+
+//close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// close it when clicked outside
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+	
+	
 	 }
+	//NEW CODE FOR MODAL STUFF 
+	 
+	 
+	 
+	 
 }
 
 function microwaveFunction() {
